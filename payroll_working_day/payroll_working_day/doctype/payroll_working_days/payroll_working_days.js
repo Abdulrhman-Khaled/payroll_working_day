957ae7d9ea70fe014d293d0f67f1e0e990ba3560
itqan <frappe@srv715558.hstgr.cloud>
2025-05-28 13:56:51 +0000

    edit

---
+++ payroll_working_day/payroll_working_day/doctype/payroll_working_days/payroll_working_days.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, bodykh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Payroll Working Days', {
+	// refresh: function(frm) {
+
+	// }
+});
